9a6a8ef9f436a604b3871edc3d81ec31c03b3ca7
Junseo <lowapple99@gmail.com>
2018-01-15 18:25:50 +0900

    Fixed

--- app.py
+++ app.py
@@ -51,9 +51,7 @@ class ScrapKgsh:
         next_title = element_item.find('a')['title']
 
         facebook_message = next_title
-
-	    print(next_title)
-
+        
         next_title = str(next_title.encode('utf-8'))
 
         print(next_title)
